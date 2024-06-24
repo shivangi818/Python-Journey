84ae1adb32a82cc2854114d9cff043d1e59cc70e
Shivangi Sahay <141395248+shivangi818@users.noreply.github.com>
2024-06-24 20:47:27 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,136 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(1,2,3,4)
+1 2 3 4
+print(1,2,3, sep'/')
+SyntaxError: invalid syntax
+print(1,2,3, sep='/')
+1/2/3
+print(1,2,3, sep='jai mata di')
+1jai mata di2jai mata di3
+print(hello)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(hello)
+NameError: name 'hello' is not defined. Did you mean: 'help'?
+print('hello')
+hello
+print(1, end="jai shree ram")
+1jai shree ram
+fnum=100
+fnum
+100
+fnum
+100
+a,b,c=2,3,4
+a
+2
+b
+3
+c
+4
+5+4.5
+9.5
+#hi
+#hello
+ a=3
+ 
+SyntaxError: unexpected indent
+a=3
+a=0101
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+a=0b111
+a
+7
+a=0x123
+a
+291
+a=1.5e4
+a
+15000.0
+city="Kolkata"
+city
+'Kolkata'
+city='kolkata'
+city
+'kolkata'
+x=None
+x
+
+x
+x=4
+x
+4
+2+3
+5
+3+7
+10
+2/2
+1.0
+4*7
+28
+3%3
+0
+3-3
+0
+**4
+SyntaxError: invalid syntax
+2**4
+16
+3>2
+True
+4<3
+False
+3>=0
+True
+>>> 3==3
+True
+>>> 6==5
+False
+>>> 3!=2
+True
+>>> 1 and 0
+0
+>>> 1 or 0
+1
+>>> not 1
+False
+>>> True+4
+5
+>>> False+3
+3
+>>> 2|3
+3
+>>> 2&3
+2
+>>> 2^3
+1
+>>> 4>>2
+1
+>>> 2<<6
+128
+>>> ~2
+-3
+>>> a=2
+>>> a
+2
+>>> a=2
+>>> a+=2
+>>> a
+4
+>>> a**=2
+>>> a
+16
+>>> a
+16
+>>> a++
+SyntaxError: incomplete input
+>>> ++a
+16
+>>> city="Kolkata"
+>>> 'Kol' in city
+True
+>>> 'ata' in city
+True
+>>> 'xyz' not in city
+True
