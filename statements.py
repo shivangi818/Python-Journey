3d1e86e95db9fce64fb3e575f9cf23ba89b54032
Shivangi Sahay <141395248+shivangi818@users.noreply.github.com>
2024-06-27 12:58:08 +0530

    Add files via upload

---
+++ statements.py
@@ -0,0 +1,59 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Users/shiva/OneDrive/Desktop/for.py ===================
+1
+2
+3
+
+==================== RESTART: C:/Users/shiva/OneDrive/Desktop/for.py ===================
+1
+2
+3
+4
+5
+6
+for i in range(1,11):
+    if i ==7:
+        break
+    print(i)
+
+1
+2
+3
+4
+5
+6
+for i in range(1,11):
+    print(i)
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> for i in range(1,11):
+...     if i ==6
+...     
+SyntaxError: incomplete input
+>>> for i in range(1,11):
+...     if i ==6:
+...         continue
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+5
+7
+8
+9
+10
