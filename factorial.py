565ab25d414df42b5e61165c4b7da07d9c0bd36a
Shivangi Sahay <141395248+shivangi818@users.noreply.github.com>
2024-07-06 20:56:18 +0530

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,8 +1,15 @@
-def factorial(n):
-    if n==1:
+#Write a program to find the factorial o#f a number.
+
+#Factorial of n is:
+
+#n! = n * (n-1) * (n-2) * (n-3)....* 1
+
+#Output the factorial of 'n'. If it does not exist, output 'Error'
+
+def fact(n):
+    if n==0 or n==1:
         return 1
     else:
-        return n*factorial(n-1)
-print(factorial(6))
-
-    
+        return n*fact(n-1)
+n=int(input())
+print(fact(n))
