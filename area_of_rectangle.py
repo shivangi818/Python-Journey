ecfb165f9f134e90b946702f3901ba2fe5f6e74f
Shivangi Sahay <141395248+shivangi818@users.noreply.github.com>
2024-07-06 20:56:04 +0530

    Add files via upload

---
+++ area_of_rectangle.py
@@ -0,0 +1,9 @@
+def area(length, breadth):
+    # Calculate the area
+    area_ = length * breadth
+    return area_
+
+# Example usage:
+length = int(input("Enter the length: "))
+breadth = int(input("Enter the breadth: "))
+print("Area:", area(length, breadth))  # This will print the calculated area
