a3d2921b3c900b6f7e760a23d686d954df27a5af
Shivangi Sahay <141395248+shivangi818@users.noreply.github.com>
2024-07-06 20:56:44 +0530

    Add files via upload

---
+++ name_n_age.py
@@ -0,0 +1,12 @@
+name = input()
+age = input()
+print("The name of the person is", name, "and the age is", age +".")
+
+
+#Sample Input 1:
+#Ali
+#30
+#Sample Output 1:
+#The name of the person is Ali and the age is 30.
+#Explanation of Sample Input 1:
+#The input name is Ali and the input age is 30 which is printed in the specified format.
